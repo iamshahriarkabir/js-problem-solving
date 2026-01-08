365c4e803f4e85546be1dcf851461f3458cf7b04
zafor <z4codelearn@gmail.com>
2026-01-08 18:51:53 +0600

    Capitalize first letter of each word in a string

---
+++ problem8_capitalizeWords.js
@@ -0,0 +1,13 @@
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let word of words) {
+    result.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return result.join(" ");
+}
+
+// Example
+console.log(capitalizeWords("hello world")); 
+// "Hello World"
